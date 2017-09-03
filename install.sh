ccde7e63b420962bd375d03443f0ea074beb76d5
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2017-09-03 22:47:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mv ~/.config/nvim ~/.config/nvim_bak
+curl -fLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/xiaohuilam/vimrc/master/.vimrc
